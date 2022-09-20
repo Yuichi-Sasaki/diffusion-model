17b2153a55532eb30cf706c71321243d6a25954f
Yuichi-Sasaki <y_sasaki@neuralpocket.com>
2022-09-20 09:27:10 +0900

    minor

--- DiffusionModel/DiffusionModel.py
+++ DiffusionModel/DiffusionModel.py
@@ -189,7 +189,7 @@ class DiffusionModel(object):
                         xt = x0 * coeff_x0 + noise_gt * coeff_noise
                         # Note: 大体、-4 ~ +4くらいの範囲になる。果たしてこれは正しい振る舞いか？
                         if self.do_clip_noise:
-                            torch.clamp(xt, min=-1., max=+1.)
+                            xt = np.clip(xt, -1., +1.)
 
                         #print(t,xt.min(),xt.max(), coeff_x0, coeff_noise)
 
