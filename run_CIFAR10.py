31679f5321b28f4ba33dfe53af60d71df0043485
Yuichi-Sasaki <y_sasaki@neuralpocket.com>
2022-10-31 16:55:47 +0900

    cleaned up the files

--- run_CIFAR10.py
+++ run_CIFAR10.py
@@ -1,18 +1,24 @@
 import argparse
 
 import torchvision
+import torch
 
 from DiffusionModel.DiffusionModel import DiffusionModel
 from DiffusionModel.UNet import UNet
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--working_dir", type=str, default="working/CIFAR10")
-parser.add_argument("--gpu", type=int, default="-1")
+parser.add_argument("--working_dir", type=str, default="working/cifar10")
+parser.add_argument("--gpu", type=str, default="-1")
 parser.add_argument("--batch_size", type=int, default=128)
+parser.add_argument("--lr", type=float, default=2e-4)
+parser.add_argument("--ema_decay", type=float, default=None)
+parser.add_argument("--load", type=str, default=None)
 args = parser.parse_args()
 
 # UNetを作成
 model = UNet(in_channels=3, enable_time_embedding=True)
+if args.load is not None:
+    print(model.load_state_dict(torch.load(args.load)))
 
 # DiffusionModelを作成
 diff = DiffusionModel(
@@ -33,11 +39,12 @@ dataset = torchvision.datasets.CIFAR10(
 # 学習を実行
 diff.train(
     dataset,
-    epochs=100,
+    epochs=200,
     batch_size=args.batch_size,
-    lr=2e-4,
+    lr=args.lr*diff.n_gpus,
     plot_timesteps=[500, 750, 900, 990, 999],
-    save_freq=10,
-    generate_freq=5,
-    loss_type="l2",
+    save_freq=1,
+    generate_freq=1,
+    num_workers=diff.n_gpus*4,
+    ema_decay=args.ema_decay
 )
